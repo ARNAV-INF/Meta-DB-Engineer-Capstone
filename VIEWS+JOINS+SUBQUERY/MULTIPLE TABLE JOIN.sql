a7741d180c57c7900e85c96e10cdee904325aa03
Arnav Ahuja <mail.ahujaarnav@gmail.com>
2026-05-01 20:03:35 +0530

    Edited file code

--- VIEWS+JOINS+SUBQUERY/MULTIPLE TABLE JOIN.sql
+++ VIEWS+JOINS+SUBQUERY/MULTIPLE TABLE JOIN.sql
@@ -1,4 +1,5 @@
 use little_lemon_db;
+CREATE VIEW "CustomerDetails" AS
 select Customers.CustomerID, concat(Customers.FirstName,' ',Customers.LastName) as "FullName",
 orders.OrderID, orders.TotalCost,
 Menu.Category as "MenuName",
