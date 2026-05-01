e349559b3bf65e3b77f4eed748ea4439f74c3f34
Arnav Ahuja <mail.ahujaarnav@gmail.com>
2026-05-01 20:10:14 +0530

    Rename view to CustomerDetailsView and update query

--- VIEWS+JOINS+SUBQUERY/MULTIPLE TABLE JOIN.sql
+++ VIEWS+JOINS+SUBQUERY/MULTIPLE TABLE JOIN.sql
@@ -1,5 +1,5 @@
 use little_lemon_db;
-CREATE VIEW CustomerDetails AS
+CREATE VIEW CustomerDetailsView AS
 select Customers.CustomerID, concat(Customers.FirstName,' ',Customers.LastName) as "FullName",
 orders.OrderID, orders.TotalCost,
 Menu.Category as "MenuName",
@@ -7,4 +7,4 @@ menuitems.ItemName, menuitems.ItemCategory
 from customers inner join orders inner join menu inner join menuitems
 on customers.customerid = orders.customerid and orders.menuid = menu.MenuID and menu.MenuItemsID = menuitems.MenuItemsID
 where orders.TotalCost > 150 order by totalcost asc;
-select * from CustomerDetails;
+select * from CustomerDetailsView;
