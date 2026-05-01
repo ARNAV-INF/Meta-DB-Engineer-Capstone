1a9167a0282fab382bf03063fe8ae33e83d783ce
Arnav Ahuja <mail.ahujaarnav@gmail.com>
2026-05-01 20:08:58 +0530

    Create MenuDetails view for menu categories
    Created a view 'MenuDetails' to select menu categories based on order quantity.

--- VIEWS+JOINS+SUBQUERY/SUBQUERY.sql
+++ VIEWS+JOINS+SUBQUERY/SUBQUERY.sql
@@ -2,3 +2,4 @@ use little_lemon_db;
 CREATE VIEW MenuDetails AS
 select menu.Category as "MenuName" from menu
 where MenuID = ANY(select MenuID from orders where quantity > 2);
+select * from MenuDetails;
